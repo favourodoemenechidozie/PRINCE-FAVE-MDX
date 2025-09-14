4db9479fb7be3cd7e70286c284c851206d4f1f5f
favourodoemenechidozie <odoemenefavour3@gmail.com>
2025-09-14 12:38:57 +0630

    all updated versions

--- index.js
+++ index.js
@@ -7,7 +7,6 @@
  * 
  * Credits:
  * - Baileys Library by @adiwajshing
- * - Pair Code implementation inspired by TechGod143 & DGXEON 
  */ 
 require('./settings')
 const { Boom } = require('@hapi/boom')
