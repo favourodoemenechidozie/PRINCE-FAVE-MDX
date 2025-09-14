f89535e52cc32c25ccf79ffba49d49adaf083bc2
favourodoemenechidozie <odoemenefavour3@gmail.com>
2025-09-14 09:18:05 +0630

    first commit

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'PRINCE FAVE MDX',
+  author: '‎',
+  botName: "PRINCE FAVE MDX",
+  botOwner: 'Prince (C.O TECH)', // Your name
+  ownerNumber: '2349129279369', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing groups and giving you ultimate advantages over other mortals. Enjoy the bot and have fun.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
